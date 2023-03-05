6b254a185ef2c79b6b96237d11486a9b42f9eaa7
Aluel-kuot <maryaluelkuot@gmail.com>
2023-03-05 15:13:39 +0300

    student

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    var nameofstudent = "Gidion"
+    println(nameofstudent)
+    var ageofstudent = 18
+    println(ageofstudent)
+    var weightofstudent =56
+    println(weightofstudent)
+    var phonenumber ="0785634986"
+    println(phonenumber)
+    var kenyanitizenship: Boolean =false
+    var kenyancitizenship: Boolean =true
+
+}
